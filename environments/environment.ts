8be25db7bcff4113fa02fb533616dfc9f1b13f6d
Einstein Yap <einsteinyap2014@gmail.com>
2021-07-14 17:42:10 +0800

    Production is done

--- environments/environment.ts
+++ environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
     production: false,
-    apiUrl:'https://einstein-shop-backend.herokuapp.com/api/v1/'
-    // apiUrl: 'http://localhost:3000/api/v1/'
+    // apiUrl:'https://einstein-shop-backend.herokuapp.com/api/v1/'
+    apiUrl: 'http://localhost:3000/api/v1/'
 };
 
 /*
